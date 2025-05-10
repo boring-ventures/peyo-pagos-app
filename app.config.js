496ecb1e2aa1a2229eaee2aa7d4e8225f61ee178
IntiRraptor <99521721+IntiRraptor@users.noreply.github.com>
2025-05-10 07:53:42 -0400

    fix: updated project id & slug

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "Boring-Template-Expo",
-  slug: "boring-template",
+  slug: "boring-template-expo",
   version: "1.0.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
@@ -29,9 +29,10 @@ module.exports = {
   web: {
     favicon: "./assets/images/favicon.png"
   },
+  owner: "boring-ventures",
   extra: {
-    eas: {
-      projectId: "your-eas-project-id"
+    "eas": {
+      "projectId": "8ba01203-a27b-4497-8c25-16641de49cb8",
     },
     EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
     EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
